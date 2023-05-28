6b41d75d98615ac534083407a1e1dd84ed9813f5
Kristoffer Moberg Christensen <kristoffermoberg.christensen@skatteetaten.no>
2023-05-28 23:51:13 +0200

    test: fix failing

--- src/test/kotlin/dev/krismoc/mealer/controller/MealPlanControllerTest.kt
+++ src/test/kotlin/dev/krismoc/mealer/controller/MealPlanControllerTest.kt
@@ -62,7 +62,7 @@ class MealPlanControllerTest : AbstractAuthenticatedControllerTest() {
             }
 
         }.andExpect {
-            status { isOk() }
+            status { isCreated() }
             jsonPath("$.id") { value(1) }
             jsonPath("$.weekNumber") { value(1) }
             jsonPath("$.year") { value(2022) }
